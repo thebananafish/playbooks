3d6afe86850d5a97b7cd2476644b03d575d38b5b
bananafish <bananafish@installgentoo.com>
2013-11-26 15:20:12 -0500

    changes made, final test ready

--- werc-apache2/src/apache9.sh
+++ werc-apache2/src/apache9.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# ansible probably has a better way to do this but for now...
 cp src/werc /etc/apache2/sites-available/
 a2ensite werc
 a2dissite default
-
+a2enmod rewrite
 HOSTNAME=`hostname -f`
 
 mv /var/werc/sites/werc.cat-v.org /var/werc/sites/$HOSTNAME
